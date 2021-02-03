da624a4dccb199f4985615adeab1156f913fe2c7
Samikshya <samikshyaaryal5@gmail.com>
2021-02-03 20:42:34 +0200

    re-factoring the code

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,38 +2,66 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+  { name: 'Arto Hellas', number: '040-123456' }
   ])
   const [ newName, setNewName ] = useState('')
-
-  const handleChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+  const [ newNumber, setNewNumber ] = useState('')
+  const [newSearch, setNewSearch] = useState([])
+  
+  const compare = persons.filter((item)=> item.name === newName)
 
   const onSubmit = (event) => {
     event.preventDefault()
     const nameObject ={
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    console.log(nameObject)
-    setPersons(persons.concat(nameObject))
+    if (compare.length === 0){
+      setPersons(persons.concat(nameObject)) 
+      
+    }else{ alert(`${newName} is already in the phonebook`)}
+    
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  };
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  };
+
+  const handleNewSearch = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value)
   }
-  const name = () => persons.map(person => <p>{person.name}</p>)
+
+  const search = persons.filter(person => person.name === newSearch)
+   
+  const name_row = () => persons.map(person => <p>{person.name}</p>)
+  const number_row = () => persons.map(person => <p>{person.number}</p>)
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with: <input value ={newSearch} onChange={handleNewSearch}  on/></div>
+      <div>filtered list :</div>
+      <div>{ search.map((item)=> <li>{ item.name} {item.number}</li>)}</div>
+      <hr />
       <form onSubmit ={onSubmit}>
         <div>
-          name: <input value ={newName} onChange={handleChange}  />
+          name: <input value ={newName} onChange={handleNameChange}  />
+          number: <input value ={newNumber} onChange ={handleNumberChange}/>
         </div>
         <div>
           <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-       <div>{name()}</div>
+       <div>{name_row()} {number_row()}</div>
     </div>
   )
 }
--- part2/phonebook/test.js
+++ part2/phonebook/test.js
@@ -1,56 +1,78 @@
-// const persons = [{
-//   name: "sujan Pokharel"
-// }]
+const persons = [{
+  name: "sujan Pokharel"
+}]
 
 // persons.push("samikshya")
 // persons.push("abb")
-// persons.push({hello: "what is it"})
+// persons.push({
+//   hello: "what is it"
+// })
 
 // console.log(persons)
+const leen = ["1", 2, 3].length
+console.log(leen)
 
 
-import React, { useState } from 'react'
-import Note from './components/Note'
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    'a new note...'
-  ) 
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
-    }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-
-      <form onSubmit={addNote}>
-         <input value={newNote}   onChange={handleNoteChange}/>
-        <button type="submit">save</button>
-      </form>
-    </div>
-  )
+const name = {
+  name: "sujan Pokharel"
 }
 
-export default App 
+name.filter( )
+
+// const output = persons.filter((persons.name === name.name))
+// console.log(output)
+
+// const compare = () => {
+//   if (persons[0].name === name.name) {
+//     alert(`name already exiits`)
+
+//   }
+//   return
+// }
+// (compare())
+
+// import React, { useState } from 'react'
+// import Note from './components/Note'
+
+// const App = (props) => {
+//   const [notes, setNotes] = useState(props.notes)
+//   const [newNote, setNewNote] = useState(
+//     'a new note...'
+//   ) 
+
+//   const addNote = (event) => {
+//     event.preventDefault()
+//     const noteObject = {
+//       content: newNote,
+//       date: new Date().toISOString(),
+//       important: Math.random() < 0.5,
+//       id: notes.length + 1,
+//     }
+//     setNotes(notes.concat(noteObject))
+//     setNewNote('')
+//   }
+
+//   const handleNoteChange = (event) => {
+//     console.log(event.target.value)
+//     setNewNote(event.target.value)
+//   }
+
+
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => 
+//           <Note key={note.id} note={note} />
+//         )}
+//       </ul>
+
+//       <form onSubmit={addNote}>
+//          <input value={newNote}   onChange={handleNoteChange}/>
+//         <button type="submit">save</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App 
